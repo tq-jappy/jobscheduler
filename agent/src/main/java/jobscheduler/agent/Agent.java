fdc788391354cccab3c903268fad22fadf7654ea
Takumi Endo <tq-ed@v7.com>
2014-09-10 20:40:02 +0900

    write main as jersey JAX-RS http server.

--- agent/src/main/java/jobscheduler/agent/Agent.java
+++ agent/src/main/java/jobscheduler/agent/Agent.java
@@ -1,12 +1,31 @@
 package jobscheduler.agent;
 
+import jobscheduler.agent.resource.v1.AgentResource;
+
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.jersey.api.core.ClassNamesResourceConfig;
+import com.sun.net.httpserver.HttpServer;
+
 /**
  * 
  * @author t_endo
  */
 public class Agent {
 
-    public static void main(String...args) {
-        
+    public static void main(String... args) throws Exception {
+        ClassNamesResourceConfig config = new ClassNamesResourceConfig(
+                AgentResource.class);
+        config.getProperties().put(
+                "com.sun.jersey.spi.container.ContainerRequestFilters",
+                "com.sun.jersey.api.container.filter.LoggingFilter");
+
+        HttpServer server = HttpServerFactory.create("http://localhost:4444/",
+                config);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            server.stop(0);
+        }));
+
+        server.start();
     }
 }
--- agent/src/main/java/jobscheduler/agent/resource/v1/AgentResource.java
+++ agent/src/main/java/jobscheduler/agent/resource/v1/AgentResource.java
@@ -1,13 +1,16 @@
 package jobscheduler.agent.resource.v1;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 /**
  * @author t_endo
@@ -15,10 +18,16 @@ import javax.ws.rs.core.Response;
 @Path("/")
 public class AgentResource {
 
+    @Context
+    private UriInfo context;
+
+    // @Context
+    // private HttpServletRequest httpServletRequest;
+
     @GET
     @Path("/status")
     public Response getStatus() {
-        return Response.ok().build();
+        return Response.ok().entity("OK").build();
     }
 
     @GET
@@ -28,14 +37,16 @@ public class AgentResource {
     }
 
     @POST
-    @Path("/jobs/{id}")
-    public Response startJob(@Context HttpServletRequest req,
-            @PathParam("id") String id) {
-        String remoteHost = req.getRemoteHost();
-        String remoteAddr = req.getRemoteAddr();
-        int remotePort = req.getRemotePort();
-        String msg = String.format("%s - %s (%s:%d)", id, remoteHost,
-                remoteAddr, remotePort);
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/jobs")
+    public Response startJob(String obj) {
+        // String remoteHost = httpServletRequest.getRemoteHost();
+        // String remoteAddr = httpServletRequest.getRemoteAddr();
+        // int remotePort = httpServletRequest.getRemotePort();
+        // String msg = String.format("%s (%s:%d)", remoteHost, remoteAddr,
+        // remotePort);
+        String msg = "test";
 
         return Response.ok(msg).build();
     }
